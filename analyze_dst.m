f31ac4a9d25435a40b2147f572a7f12ebd31c5e1
Veronica Scerra <vscerra@gmail.com>
2023-11-13 12:59:10 -0800

    Updating analyze_dst and plotting
    Update of analyze_dst and plotting script plot_histogram

--- analyze_dst.m
+++ analyze_dst.m
@@ -14,44 +14,34 @@ oplusa = trialTab(:,5);
 rtData = trialTab(:,6);
 saccadeStart = trialTab(:,9);
 goTime = trialTab(:,8);
-fileNo = trialTab(:,11);
-trialNo = trialTab(:,10);
-ccorrect = trialTab(:,7);
-pcorrect = trialTab(:,12);
 
  % In aborted trials
 oplusa_ab = tabAb2(:,5);
 rtData_ab = tabAb2(:,6);
 saccadeStart_ab = tabAb2(:,9);
 goTime_ab = tabAb2(:,8);
-fileNo_ab = tabAb2(:,11);
-trialNo_ab = tabAb2(:,10);
-ccorrect_ab = tabAb2(:,7);
-pcorrect_ab = tabAb2(:,12);
 
 % analysis parameters
-rtBins = 0:10:400;
+rtBins = [0, 400, 10];
 
 
 
 %% === Reaction time analysis ===
-rtBins = [0, 400, 10];
+
 % Plot the histograms for Reaction Time
-figure(1); hold on
-subplot(121)
+figure(1); clf; hold on
 [nRt, xRt, mean_rt] = plot_histogram(rtData, rtBins, @mean);
 hold on
 formatted_rt = sprintf('%.2f',mean_rt); 
-textMssg = ['mean reaction time of ', formatted_rt , ' for correct DST trials'];
+textMssg = ['mean reaction time of ', formatted_rt , 'ms for correct DST trials'];
 title(textMssg)
-xlabel('reaction time [ms]')
 
-subplot(122)
-[nRtAb, xRtAb, mean_rtAb] = plot_histogram(rtData_ab, rtBins, @mean);
-hold on
+% Plot error trials on same graph
+[nRtAb, xRtAb, mean_rtAb] = plot_histogram(rtData_ab, rtBins, @mean, 'r', 1);
 formatted_rt = sprintf('%.2f',mean_rtAb); 
-textMssg = ['mean reaction time of ', formatted_rt , ' for error DST trials'];
-title(textMssg)
+textMssg = ['mean reaction time of ', formatted_rt , 'ms for error DST trials'];
+ys = ylim;
+text(200, ys(2)-(ys(2)*.05), textMssg, 'VerticalAlignment','bottom','HorizontalAlignment','center','Color','r')
 xlabel('reaction time [ms]')
 
 hold off
@@ -62,19 +52,38 @@ lowRtAb = find(xRtAb < 6);
 %% === OPlusA analysis ===
 opaBins = [100, 700, 50];
 % Plot histograms for O + A data
-figure(2); hold on
-subplot(121)
+figure(2); clf; hold on
 [nOpa, xOpa, med_opa] = plot_histogram(oplusa, opaBins, @median);
-hold on
 formatted_opa = sprintf('%.0f',med_opa); 
-textMssg = ['median reaction time of ', formatted_opa , ' for correct DST trials'];
+textMssg = ['median O + A of ', formatted_opa , 'ms for correct DST trials'];
 title(textMssg)
-xlabel('O + A time [ms]')
 
-subplot(122)
-[nOpaAb, xOpaAb, med_opaAb] = plot_histogram(oplusa_ab, opaBins, @median);
-hold on
+% Plot aborted trials in same graph
+[nOpaAb, xOpaAb, med_opaAb] = plot_histogram(oplusa_ab, opaBins, @median, 'r', 1);
 formatted_opa = sprintf('%.0f',med_opaAb); 
-textMssg = ['median reaction time of ', formatted_opa , ' for correct DST trials'];
-title(textMssg)
+textMssg = ['median O + A of ', formatted_opa , 'ms for error DST trials'];
+ys = ylim;
+text(400, ys(2)-(ys(2)*.05), textMssg, 'VerticalAlignment','bottom','HorizontalAlignment','center','Color','r')
 xlabel('O + A time [ms]')
+hold off
+
+%% === Go-Saccade offset ===
+figure(3); clf;
+gsOffset = saccadeStart - goTime;
+gsOffsetAb = saccadeStart_ab - goTime_ab;
+gsoBins = [0,200, 10];
+[nGSO, xGSO, mean_GSO] = plot_histogram(gsOffset, gsoBins, @mean);
+formatted_gso = sprintf('%.2f',mean_GSO); 
+hold on
+textMssg = ['mean Go-Saccade offset of ', formatted_gso , 'ms for correct DST trials'];
+title(textMssg)
+
+% Plot error trials on same graph
+[nGSOab, xGSOab, mean_GSOab] = plot_histogram(gsOffsetAb, gsoBins, @mean, 'r', 1);
+formatted_gso = sprintf('%.2f',mean_GSOab); 
+textMssg = ['mean Go-Saccade offset of ', formatted_gso , 'ms for error DST trials'];
+ys = ylim;
+text(100, ys(2)-(ys(2)*.05), textMssg, 'VerticalAlignment','bottom','HorizontalAlignment','center','Color','r')
+xlabel('Saccade Start - Go time [ms]')
+
+
--- plot_histogram.m
+++ plot_histogram.m
@@ -1,13 +1,15 @@
-function [nData, binId, dataMetric] = plot_histogram(data, binRange, plotMetric)
+function [nData, binId, dataMetric] = plot_histogram(data, binRange, plotMetric, color, add_on)
 %PLOT_HISTOGRAM function takes input data and binRange(opt), to return a
 %figure, nData, binId, and dataMetric. 
 
-% [nData, binId, dataMetric] = plot_histogram(data, binRange, plotMetric)
+% [nData, binId, dataMetric] = plot_histogram(data, binRange, plotMetric, color, add_on)
 
 % INPUTS: 
     % data: vector of the data you want binned and plotted
     % binRange: optional 3-element vector designating the min and max of
     % the bins as well as desired bin size ([minVal, maxVal, binSize])
+    % the optional 4th input is the color you want for bar faces - default
+        % is blue
 % OUTPUTS: 
     % a figure with the histogram
     % nData: 1 x number_of_bins vector with the number of cases per bin
@@ -16,20 +18,26 @@ function [nData, binId, dataMetric] = plot_histogram(data, binRange, plotMetric)
 
 % Veronica Scerra, 11/2023
 
+if nargin < 4
+    color = [.2 0 .9];
+end
+
+
 bins = binRange(1):binRange(3):binRange(2);
 [nData, binId] = histc(data, bins);
 dataMetric = plotMetric(data);
 
-
-fact = floor(log10(max(nData)));
-y_max = (ceil(max(nData) / 10^fact)) * 10^fact;
-
-bar(bins, nData)
+bar(bins, nData,'FaceColor',color)
 hold on
 box off
 xlim([binRange(1), binRange(2)])
+
+if nargin < 5 || add_on == 0
+fact = floor(log10(max(nData)));
+y_max = (ceil(max(nData) / 10^fact)) * 10^fact;
+ylim([0, y_max])
 ylabel('number of trials')
 set(gca, 'TickDir', 'out', 'Ytick', [0, y_max/2, y_max])
-
+end
 end
 
